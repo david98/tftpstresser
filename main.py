ee5474e83b38f46066e909368e830494b8b5f02b
david98 <davidevolta98@gmail.com>
2021-02-25 17:07:57 +0100

    improvements

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import tftpy
 import sys
 from os import listdir
 from os.path import isfile, join
+from time import sleep
 
 
 def upload_files(client, path):
-    files_to_upload = [f for f in listdir(path) if isfile(join(path, f))]
+    files_to_upload = [f for f in listdir(path) if isfile(join(path, f)) and f != '.gitkeep']
     print('Files that will be uploaded: {0}'.format(files_to_upload))
 
     for file in files_to_upload:
@@ -13,6 +14,8 @@ def upload_files(client, path):
             print('Uploading "{0}"...'.format(file))
             client.upload(file, to_upload_path + file, timeout=10)
             print('Upload succesful.')
+            print('Waiting {0} seconds before initiating next transfer...'.format(secs_between_transfers))
+            sleep(secs_between_transfers)
         except KeyboardInterrupt:
             print('Aborting...')
             exit(0)
@@ -22,6 +25,8 @@ def upload_files(client, path):
 
 addr = sys.argv[1]
 iterations = 1
+secs_between_transfers = 0
+secs_between_iterations = 0
 if len(sys.argv) > 2:
     iterations = sys.argv[2]
 
@@ -31,6 +36,24 @@ if len(sys.argv) > 2:
         print('Invalid number of iterations provided.')
         exit(1)
 
+if len(sys.argv) > 3:
+    secs_between_transfers = sys.argv[3]
+
+    try:
+        secs_between_transfers = int(secs_between_transfers)
+    except ValueError:
+        print('Invalid seconds between transfers.')
+        exit(1)
+
+if len(sys.argv) > 4:
+    secs_between_iterations = sys.argv[4]
+
+    try:
+        secs_between_iterations = int(secs_between_iterations)
+    except ValueError:
+        print('Invalid seconds between iterations.')
+        exit(1)
+
 client = tftpy.TftpClient(addr, 69, options={'blksize': 512, 'timeout': 10, 'tsize': 25495})
 
 to_upload_path = './to_upload/'
@@ -38,7 +61,11 @@ to_upload_path = './to_upload/'
 if iterations < 0:
     while True:
         upload_files(client, to_upload_path)
+        print('Waiting {0} seconds before next iteration...'.format(secs_between_iterations))
+        sleep(secs_between_iterations)
 else:
     for i in range(0, iterations):
         upload_files(client, to_upload_path)
-
+        if i < iterations:
+            print('Waiting {0} seconds before next iteration...'.format(secs_between_iterations))
+            sleep(secs_between_iterations)
